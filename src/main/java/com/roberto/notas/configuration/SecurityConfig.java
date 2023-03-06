9f3ea5b283a0a2986af44a648a2f4df6767b35fe
Rolob3rto <robertooli98@gmail.com>
2023-03-06 10:10:24 +0100

    seguridad acabada

--- src/main/java/com/roberto/notas/configuration/SecurityConfig.java
+++ src/main/java/com/roberto/notas/configuration/SecurityConfig.java
@@ -1,59 +1,72 @@
 package com.roberto.notas.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.roberto.notas.services.UserService;
 
 
-
-
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
+    @Autowired
+    JWTAuthorizationFilter authorizationFilter;
+
+
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
+
+        JWTAuthenticationFilter authenticationFilter = new JWTAuthenticationFilter();
+        authenticationFilter.setAuthenticationManager(authManager);
+        authenticationFilter.setFilterProcessesUrl("/login");
+
+        http
+            .csrf().disable()
+            .authorizeRequests()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .httpBasic()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+            .addFilter(authenticationFilter)
+            .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class)
+            ;
+
+        return http.build();
     }
-    
+
     @Bean
-    UserService myUserService(){
-        
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+//        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserService myUserService() {
         return new UserService();
     }
 
     @Bean
-    public DaoAuthenticationProvider authenticationProvider(){
-        
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-        authProvider.setUserDetailsService(myUserService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-
-
-        return authProvider;
+    AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class)
+                .userDetailsService(myUserService())
+                .passwordEncoder(passwordEncoder())
+                .and()
+                .build();
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        
-        http
-            .authorizeHttpRequests()
-            .anyRequest()
-            .authenticated()
-        .and()
-            .formLogin()
-        .and()
-            .httpBasic();
-
-        return http.build();
-    }
 }
